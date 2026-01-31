ec31ead5b783c3866b7abbd7d79fa8d565122716
root <root@DESKTOP-NGNH4VC.localdomain>
2026-01-31 21:48:04 +0100

    Voir l'indexation et le Slicing

---
+++ Seance2/seance2_ex3_indexation.py
@@ -0,0 +1,17 @@
+# import
+import numpy as np
+
+# Exercice 3.1
+A = np.arange(1,10,2)
+# En Python, l'indexation commence à 0 et terminer à n-1, n est la taille d'une liste. Alors pour afficher le premier élément d'une liste, il faut afficher avec l'indexe 0.
+
+B = np.arange(0,16,1)
+print(B)
+
+B = np.reshape(B, (4, 4))
+print(B)
+B_E1 =B[0,0] # Donne 0
+B_E2= B[1]
+B_EAvantDernier = B[-2] # ou 2 au lieu de -2 (mais -2 est mieux pour afficher l'avant dernière ligne).
+SousTableau = B[0:1, 1:2]
+print(SousTableau)
