3e3bc7489ba1230a6d1addfbefcde47a9f935967
root <root@DESKTOP-NGNH4VC.localdomain>
2026-02-01 12:55:16 +0100

    Indexation et Slicing

--- Seance2/seance2_ex3_indexation.py
+++ Seance2/seance2_ex3_indexation.py
@@ -13,5 +13,9 @@ print(B)
 B_E1 =B[0,0] # Donne 0
 B_E2= B[1]
 B_EAvantDernier = B[-2] # ou 2 au lieu de -2 (mais -2 est mieux pour afficher l'avant dernière ligne).
-SousTableau = B[0:1, 1:2]
+SousTableau = B[0:2, 0:2]
 print(SousTableau)
+
+B_modifier = B
+B_modifier[-1] = 99
+print(B_modifier)
