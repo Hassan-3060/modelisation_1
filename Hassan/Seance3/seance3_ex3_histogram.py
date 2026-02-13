ce03382eea26c3a6439de637915dad5b0479e124
root <root@DESKTOP-NGNH4VC.localdomain>
2026-02-14 00:57:38 +0100

    terminer

---
+++ Hassan/Seance3/seance3_ex3_histogram.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.random.rand(1000)
+plt.figure()
+plt.hist(data, bins = 100, color = 'gray')
+plt.xlabel("data")
+plt.ylabel("frequency")
+plt.title("Histogram")
+plt.legend(['Valeur random'])
+plt.savefig('fig_s3ex3_hist.png')
+plt.show()
