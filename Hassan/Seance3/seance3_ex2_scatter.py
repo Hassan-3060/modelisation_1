59fdc44e277e3bc90e13cd71191c799ae90e8b4e
root <root@DESKTOP-NGNH4VC.localdomain>
2026-02-14 00:57:08 +0100

    Terminer

---
+++ Hassan/Seance3/seance3_ex2_scatter.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1, 2, 20)
+y = np.random.rand(20)
+data = np.column_stack((x, y))
+np.savetxt('data.txt', data, header='x, y', fmt='%.4f')
+
+dataImporter = np.loadtxt('data.txt')
+xImporter = dataImporter[:,0]
+yImporter = dataImporter[:,1]
+
+plt.figure()
+plt.scatter(xImporter, yImporter, color='red', marker='o', label='data')
+plt.title('Nuage de points')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.savefig('fig_s3ex2_scatter.png')
+plt.show()
