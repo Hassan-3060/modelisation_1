ab21d117dbcfb90acd69db2943742334d4aaf0da
root <root@DESKTOP-NGNH4VC.localdomain>
2026-02-14 00:18:44 +0100

    Tester Figup

--- Hassan/Seance3/seance3_ex2_scatter.py
+++ Hassan/Seance3/seance3_ex2_scatter.py
@@ -9,7 +9,10 @@ w = z**3
 colors = ['red', 'green', 'blue']
 
 fig = Figup(x, y, colors[0], 'Courbe de Figup', 'y = x²', 'x, y')
-fig.ajouterCourbe(y, z, colors[1], 'z =  $\\sqrt{y}$').ajouterCourbe(z, w, colors[2], 'w = $z^3$').courbe()
+#fig.ajouterCourbe(y, z, colors[1], 'z =  $\\sqrt{y}$').ajouterCourbe(z, w, colors[2], 'w = $z^3$').courbe()
 
 fig2 = Figup(x, y, colors[0], 'Courbe de Figup 2', 'y = x²', 'x, y')
-fig2.ajouterCourbe(y, z, colors[1], 'z =  $\\sqrt{y}$').courbe()
+#fig2.ajouterCourbe(y, z, colors[1], 'z =  $\\sqrt{y}$').courbe()
+
+fig3 = Figup(w, z, colors[0], None, 'w = $z^3$', 'w, z')
+fig3.courbe()
