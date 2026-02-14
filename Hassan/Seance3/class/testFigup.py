80d3cc4ce028a50643f260bf7486379c8309c58e
root <root@DESKTOP-NGNH4VC.localdomain>
2026-02-14 22:32:34 +0100

    Organisation

--- Hassan/Seance3/class/testFigup.py
+++ Hassan/Seance3/class/testFigup.py
@@ -1,4 +1,3 @@
-from matplotlib import pyplot as plt
 from Figup import *
 import numpy as np
 
--- Hassan/Seance3/scripts/seance3_ex2_scatter.py
+++ Hassan/Seance3/scripts/seance3_ex2_scatter.py
@@ -1,12 +1,14 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(1, 2, 20)
 y = np.random.rand(20)
 data = np.column_stack((x, y))
-np.savetxt('data.txt', data, header='x, y', fmt='%.4f')
+np.savetxt('../fichiers/data.txt', data, header='x, y', fmt='%.4f')
 
-dataImporter = np.loadtxt('data.txt')
+dataImporter = np.loadtxt('../fichiers/data.txt')
 xImporter = dataImporter[:,0]
 yImporter = dataImporter[:,1]
 
@@ -17,5 +19,5 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
 plt.grid(True)
-plt.savefig('fig_s3ex2_scatter.png')
+plt.savefig('../figures/fig_s3ex2_scatter.png')
 plt.show()
